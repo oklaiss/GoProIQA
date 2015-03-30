3fa20045264a0d9778a0fffc66db7cf0ed2e660b
oklaiss <oklaiss@smu.edu>
2015-03-30 17:41:24 -0500

    Articles and Sections Iteration

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GoProIQA
 GoPro 2015 Internship Qualification Assessment
+
+#Output for Word Counter Iteration
+python parser.py constitution.txt 
+872   7652   45119 constitution.txt
+
+#Output for Not Every Word Counts Iteration
+python parser.py constitution.txt 
+all: 872   7652   45119 constitution.txt
+proper: 872   6005   40801
+
+#Output for Articles and Sections Iteration
+python parser.py constitution.txt 
+all: 872   7652   45119 constitution.txt
+proper: 872   6005   40801
+Total Articles: 07
+Total Sections: 21
+Total Sections per Article:
+   Article 1: 10
+   Article 2: 4
+   Article 3: 3
+   Article 4: 4
+   Article 5: 0
+   Article 6: 0
+   Article 7: 0
--- parser.py
+++ parser.py
@@ -28,6 +28,37 @@ with open(file) as f:
                 numWords2 -= 1
                 numBytes2 -= len(word)
 
-
 print "all: " + str(numLines) + "   " + str(numWords) + "   " + str(numBytes) + " " + file
 print "proper: " + str(numLines2) + "   " + str(numWords2) + "   " + str(numBytes2)
+
+#Articles and Sections counter
+dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0}
+current = 1
+with open(file) as fin:
+    for line in fin:
+        if "Article 2." in line:
+            current = 2
+        if "Article 3." in line:
+            current = 3
+        if "Article 4." in line:
+            current = 4
+        if "Article 5." in line:
+            current = 5
+        if "Article 6." in line:
+            current = 6
+        if "Article 7." in line:
+            current = 7
+        if "Section 1" in line or "Section 2" in line or "Section 3" in line or "Section 4" in line or "Section 5" in line or "Section 6" in line or "Section 7" in line or "Section 8" in line or "Section 9" in line or "Section 10" in line or "Section 11" in line or "Section 12" in line or "Section 13" in line:
+            dict[current] += 1
+
+print "Total Articles: " + "07"
+total = dict[1] + dict[2] + dict[3] + dict[4] + dict[5] + dict[6] + dict[7]
+print "Total Sections: " + str(total)
+print "Total Sections per Article:"
+print "   Article 1: " + str(dict[1])
+print "   Article 2: " + str(dict[2])
+print "   Article 3: " + str(dict[3])
+print "   Article 4: " + str(dict[4])
+print "   Article 5: " + str(dict[5])
+print "   Article 6: " + str(dict[6])
+print "   Article 7: " + str(dict[7])
